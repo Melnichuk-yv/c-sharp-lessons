1f75e0c75e176e6587e332615f5cad039ea6f2cb
Yuliia Melnichuk <gromova.yv@gmail.com>
2022-10-31 17:36:59 +0200

    Вывод на экран сообщение "Привет, " + name

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+
+
 Console.WriteLine("Hello, Ukraine!");
+string name;
+Console.WriteLine("Enter your name, please: ");
+name = Console.ReadLine();
+Console.WriteLine($"Hello {name}");
